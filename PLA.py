ec9209b6996fa75a18e2ad81232a6c91366ccaeb
Igor Vaz <ivaz567@hotmail.com>
2016-04-11 17:59:07 -0300

    correcao do construtor e inclusao do return na funcao de scalarMult

--- PLA.py
+++ PLA.py
@@ -1,7 +1,6 @@
 
 class PLA:
-    w = []
-    def _init_(self):
+    def __init__(self):
         self.w = [0,3,5]
        
     def sign(self,num):
@@ -23,27 +22,25 @@ class PLA:
     def scalarMult(self, a, vec):
         for i in range(len(vec)):
             vec[i] = vec[i]*a
+        return vec
         
     def fit(self,X,y):
         cont = 0
         iteration = 0
+
         for i in range(len(X)):
             X[i].insert(0,1)
+
         while True:
             cont = 0
-            print self.w
             
             for i in range(len(X)):
                 if self.sign(self.vectorMult(self.w, X[i])) != self.sign(y[i]):
-                    coco = self.vectorSum(self.w, self.scalarMult(y[i],X[i]))
-                    #self.w = self.w + y[i]*X[i]
+                    self.w = self.vectorSum(self.w, self.scalarMult(y[i],X[i]))
                     cont+=1
-                   # print "count: " + str(cont)
-            if cont == 0 or iteration == 10000:
+            if cont == 0 :
                 break
             iteration+=1
-            
-
 
         return self.w
     
@@ -52,18 +49,16 @@ class PLA:
             return 1
         else:
             return -1
-    
-    
-
 
 xTrain = [[0,0],[2,2],[4,3],[6,2],[8,1]]
 yTrain = [1, 1, -1, -1, -1]
 test = PLA()
-print "sinal de " + str(yTrain[2]) + "eh" + str(test.sign(yTrain[2]))
+
 newW = test.fit(xTrain, yTrain)
 print(newW)
 
-#print(test.predict(xTrain[2]))
+for i in range(len(xTrain)):
+    print(test.predict(xTrain[i]))
 
 
 
