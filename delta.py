add404d5942d6fa70c4c45b192dd1a7f170c1dd6
Igor Vaz <ivaz567@hotmail.com>
2016-04-18 17:19:42 -0300

    leTeste e fit

--- delta.py
+++ delta.py
@@ -1,30 +1,61 @@
-#!/usr/bin/env python
 
 import numpy as np
 import random
+import csv
 
 class Delta:
-	def __init__(self, eta=0.05, tolerancia=0.1):
-		self.tolerancia = tolerancia
-		self.eta = eta
-		self.w = []
+    def __init__(self, eta=0.05, tolerancia=0.1):
+        self.tolerancia = tolerancia
+        self.eta = eta
+        self.w = []
+        self.w0 = random.uniform(-10,10)
+        self.y = []
 
-	def run(self, X):
-		for i in range(len(X)):
-			self.w[i] = random.randrange(-10,10)
-		erroAnterior = 0
-		variacaoErro = 1
-		while variacaoErro > self.tolerancia:
-			erroTotal = 0
-			for i in range(len(X)):
-				yEstimado = 1 / (1 - np.exp(np.dot(self.w, X[i])))
-				erro = np.abs(y[i] - yEstimado)
-				erroTotal += erro
-				self.w = ajustaPlano(self.w, erro, X[i])
-				self.w0 = self.w0 * self.eta * erro
-			variacaoErro = np.abs(erroAnterior - erroTotal)
-			erroAnterior = erroTotal
+    def leTreino(self):
+        self.X = []
+        cr = csv.reader(open("dataset.csv","rb"))
+        for row in cr:
+            row = row[0].split(';')
+            for j in range(len(row)):
+                row[j] = float(row[j])
+            self.y.append(row.pop())
+            self.X.append(row)
+        self.w = [random.uniform(-10,10) for i in xrange(len(row))]
+        
+    def leTeste(self):
+        self.X = []
+        cr = csv.reader(open("test.csv","rb"))
+        for row in cr:
+            row = row[0].split(';')
+            for j in range(len(row)):
+                row[j] = float(row[j])
+            self.X.append(row)
 
-	def ajustaPlano(self, w, erro, xi):
-		w = w + erro * self.eta * xi
-		return w
+    def fit(self):
+        erroAnterior = 0
+        variacaoErro = 1
+        while variacaoErro > self.tolerancia:
+            erroTotal = 0
+            for i in range(len(self.X)):
+                print self.w
+                print self.X[i]
+                yEstimado = 1 / (1 - np.exp(np.dot(self.w, self.X[i])))
+                erro = np.abs(self.y[i] - yEstimado)
+                erroTotal += erro
+                self.w = self.ajustaPlano(self.w, erro, self.X[i])
+                self.w0 = self.w0 * self.eta * erro
+            variacaoErro = np.abs(erroAnterior - erroTotal)
+            erroAnterior = erroTotal
+
+    def ajustaPlano(self, w, erro, xi):
+        w = w + erro * self.eta * xi
+        return w
+
+    def predict(self):
+        pass
+
+delta = Delta()
+delta.leTreino()
+delta.fit()
+delta.leTeste()
+delta.predict()
